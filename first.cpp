f351b8b25619879d97ebf996bf5111b6773f43ef
Pravesh maurya <praveshm783@gmail.com>
2020-12-23 21:20:06 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+      string a, b, c;
+      cout<<"Input Three String"
+     cin >> a >> b >> c;
+     cout<<a[0]<<"."<<b[0]<<"."<<c;
+		
+	return 0;
+}
